fb58ebd958c7571832b7162421c2bf0b114592da
DanielPini <daniel@danielpini.com>
2025-02-15 14:42:09 +0100

    Added regex

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,3 +1,21 @@
+const searchInput = document.getElementById("search");
+const submitBtn = document.getElementById("submitBtn");
+const beachRegExMatch = /beach/;
+const templeRegExMatch = /temple/;
+const countryRegExMatch = /countr/;
+
+searchInput.addEventListener("keypress", (e) => {
+  if (e.key !== "Enter") {
+    return;
+  } else {
+    console.log(testKeyword(searchInput.value.toLowerCase()));
+  }
+});
+
+submitBtn.addEventListener("click", () => {
+  console.log(testKeyword(searchInput.value.toLowerCase()));
+});
+
 fetch("travel_recommendation_api.json")
   .then((response) => {
     if (!response.ok) {
@@ -7,8 +25,45 @@ fetch("travel_recommendation_api.json")
     return response.json();
   })
   .then((data) => {
-    console.log(data.countries);
+    Object.entries(data).forEach(([category, items]) => {
+      console.log(category.toUpperCase());
+      items.forEach((item) => {
+        if (item.cities) {
+          item.cities.forEach((city) => {
+            console.log(city.name, city.imageUrl);
+          });
+        } else {
+          console.log(item.name, item.imageUrl);
+        }
+      });
+    });
   })
   .catch((error) => {
     console.error("Error", error);
   });
+
+function testKeyword(input) {
+  let result = "";
+  if (!input) {
+    return;
+  }
+  if (
+    !(
+      input.match(beachRegExMatch) ||
+      input.match(templeRegExMatch) ||
+      input.match(countryRegExMatch)
+    )
+  ) {
+    return "Unknown";
+  }
+  if (input.match(beachRegExMatch)) {
+    result += "Beaches ";
+  }
+  if (input.match(templeRegExMatch)) {
+    result += "Temples ";
+  }
+  if (input.match(countryRegExMatch)) {
+    result += "Countries ";
+  }
+  return result;
+}
